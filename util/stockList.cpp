5ac215aa6045d96b6aae652f7c1301a8b620403d
Arthur Vargas <ahvargas92@gmail.com>
2017-11-22 14:15:19 -0500

    Add functionality to StockList. Including add, sort, and remove functionality

--- util/stockList.cpp
+++ util/stockList.cpp
@@ -3,126 +3,180 @@
 */
 #include "securitiesList.h"
 
+StockList::StockList( Stock s, Node *prev, Node *succ )
+{
+  head = new Node( Stock stk{"","",0,0}, prev, succ );
+  head->succ = new Node( Stk stk{"","",0,0}, head, NULL);
+  head->prev = new Node( Stock stk{s}, NULL, head);
+  head = head->prev;
+  length = 3;
+}
 
 StockList::StockList( int x )
 {
-  head = new Stock("", "", 0, 0);
-  Stock *ptr = head;
+
+  head = new Node( Stock s{"","",0,0}, NULL, NULL );
+  Node *ptr = head;
   for ( int i = 0; i < x; i++ ) {
 
-    if ( ptr == head ) {
-      head->succ = new Stock("","",0,0);
-      head->succ->prev = head;
+    if ( i = 0 ) {
+      ptr->succ = new Node( Stock s{"","",0,0},NULL,NULL);
+      ptr->succ->prev = head;
       head->prev = NULL;
       ptr = head->succ;
     } else {
-      
-      ptr->succ = new Stock("","",0,0);
-      ptr->succ->prev = ptr;
+      ptr->succ = new Node( Stock s{"","",0,0},NULL,NULL);
+      ptr->succ->prev = ptr->prev->succ;
+      ptr->prev->succ = ptr->succ;
+      ptr = ptr->succ;
       
     }
+
+    length++;
   }
+  return head;
 
 }
 
-Stock *StockList::add( Stock *n )
+Stock *StockList::add( Node *n )
 {
   // first handle the case of when the list is empty
   if (isEmpty()) {
     head = n;
     length++;
     return head;
-  } else {
-
-    Stock *ptr = head;
-    while( n->value > ptr->value && ptr != NULL ) {
-      ptr = ptr->prev;
-    }
-    // we reached the end of the list
-    if ( ptr == NULL ) {
-      return StockList::insertAtEnd(n);
-    // if the price of the first stock is less than or equal to price of the stock being added
-    else if ( ptr == head ) {
-      return StockList::insertAtFront(n);   
-    } else {
-        n->succ = ptr;
-        ptr->prev->succ = n;
-        n->prev = ptr->prev->succ;
-        ptr->prev = n;
-        length++;
-        return n;
-    }
+  } else if ( !sorted() ) {
+    StockList::sortByPrice();  
   }
-
+  Node *ptr = head;
+  while( ptr ) {
+    if (ptr->stock.value <= n->stock.value)
+      break;
+    ptr = ptr->succ;
+  }
+  // we reached the end of the list or it is the last node in the list
+  if ( ptr == NULL || ptr->succ == NULL ) {
+    return StockList::insertAtEnd(n);
+  else if ( ptr == head ) {
+    return StockList::insertAtFront(n);   
+  } else {
+    n->succ = ptr->prev->succ;
+    n->prev = ptr->prev;
+    ptr->prev->succ = n;
+    ptr->prev = n;
+    ptr->prev->prev->succ = n;
+    length++;
+  }
+  return n;
 
 }
 
-Stock *StockList::insertAtFront( Stock *n )
+Node *StockList::insertAtFront( Node *n )
 {
   n->succ = head;
   head->prev = n;
   n->prev = NULL;
   head = n;
   return head;
+}
+
+StockList::Node *insertAtEnd( Node *n )
+{
+  Node *ptr = head;
+  while (ptr->succ) 
+    ptr = ptr->succ;
+  ptr->succ = n;
+  n->prev = ptr->prev->succ;
+  length++;
+  return n;
 
 }
 
-StockList::Stock *insert( Stock *n, std::string price )
+Node *find( double t, std::string nm )
 {
-  if ( !ascending(this,price) ) sortAscending(this, price);
-
-  Stock *ptr = head;
-  if(ptr->close >= n->close) {
-    return insertAtFront(n);
-  } else {
-    while(ptr->close < n->close)
-      ptr = ptr->succ;
-    if(ptr->succ == NULL) {
-      return insertAtEnd(n);
-    } else {
-      n->succ = getStock(ptr->close);
-      ptr->prev->succ = n;
-      n->prev = p->prev;
-      getStock(ptr->close)->prev = n;
-    }
-    length++;
-  if(p==NULL)
-  n->succ = p;
-
+  Node *ptr = head;
+  while( ptr->succ ) {
+    if( ptr->stock.value == t && ptr->stock.name == nm )
+      break;
+    ptr = ptr->succ;
+  }
+  return ptr;
 }
 
-
-Stock *find( double t, std::string nm )
+StockList::Node *sortByPrice()
 {
-  double point;
-  
-  Stock *ptr = head;
-  if( p.compare("close") == 0 )
-    point = t->close;
-  else if ( p.compare("open") == 0 )
-    point = t->open;
-  else if ( p.compare("previous close") == 0 )
-    point = t->prevClose;
-  else if ( p.compare("high") == 0 )
-    point = t->high;
-  else if ( p.compare("low") == 0 )
-    point = t->low;
-  else
-    // throw error
+  while( !isSortedAscending() ) {
 
-  if(ptr
+    if( length == 0 ) {
+      return NULL;
 
+    } else if ( length ==  1 ) {
+      return head;
 
-}
+    } else if ( length == 2 ) {
 
+      if  ( head->stock.value > head->succ->stock.value ) 
+        head = head->succ;
+      return head;
 
+    } else if ( length >= 3 ) {
+      Node *pivot = getLast();
+      Node *curr = head;
 
+      while(curr->succ) {
 
-Stock *sortByPrice( std::string p )
-{
+        if(curr->stock.value <= pivot->stock.value) { // node's stock value is less than pivot. move it to the left of pivot. 
+                                                     //insert between pivot and previous node.
+          pivot->prev->succ = curr->succ->prev;
+          Node *pvPre = pivot->prev;
+          pivot->prev = curr->succ-prev;
+          curr->succ->prev->succ = pivot;
+          pivot->prev->prev = pvPre->prev->succ;
+
+        } else if ( curr->stock.value >= pivot->stock.value ) { // node's stock value is greater than pivot's. move it to the right of pivot
+
+          if (pivot->succ == NULL) { // there are no nodes after the pivot
+            curr->prev = pivot;
+            pivot->succ = curr->succ->prev;
+            curr = curr->succ;
+          } else { // insert the node between the pivot and its successor node
+            curr->prev = pivot;
+            pivot->succ->prev = curr->succ->prev;
+            curr->succ->prev->succ = pivot->succ;
+            pivot->succ = curr->succ->prev;
+            curr = curr->succ; 
+          }
+          curr->prev = NULL;
+
+        }
     
+     }
+
+  }
+ 
+}
+
+StockList::Node *getLast()
+{
+
+  Node *ptr = head;
+  while(ptr->succ != NULL)
+    ptr = ptr->succ;
+  return ptr;
 
 
+}
+
+bool StockList::isSortedAscending()
+{
+
+  Node *ptr = head;
+  while(ptr->succ != NULL) {
+    if( ptr->stock.value > ptr->succ->stock.value )
+      return false;
+    ptr = ptr->succ;
+  }
+  return true;
 
 }
 
--- util/stockList.h
+++ util/stockList.h
@@ -1,5 +1,5 @@
 /**
-  A custom list of securities.
+  StockList is a linked list of Stocks sorted by Stock price. 
 */
 
 #include <iostream>
@@ -17,23 +17,48 @@
 
 
 class StockList{
-  StockList(const Stock *s ) : head{s} { head->prev = NULL; head->succ = NULL; }
+
+  /**
+    Constructs a StockList with a single node and assigns the head to the node passed into the parameter n.
+
+    @param n the head Node in the list
+  */
+  StockList( const Node *n ) { head = n; }
  
-  StockList(const Stock &s) : head{s} { head.prev = NULL; head.succ = NULL; } 
+  /**
+    Constructs a StockList with one Nodes and assigns the head to the reference passed into the parameter n
+    @param n a reference to the head Node in the list
+  */
+  StockList( const Node &n ) { head = n; }
+
   /**
     Constructs an empty StockList with the number of Stocks passed into the parameter. Stock price and volume are set at 0.
     @param i the number of Stocks in the list
   */
   StockList(int x);
- 
-  /**
-    Inserts the Stock into a list sorted in ascending order according to the price specified by the string  parameter.
 
-    @param n the Stock to be inserted
-    @param p the price used for sorting the list
-    @return a pointer to the inserted Security
+  /**
+    Constructs a StockList with three Nodes. Assigns one Node's Stock to the parameter s, a previous Node pointer to the parameter prev, and the successor Node 
+    pointer to the parameter succ. Head is assigned to the new Node. The length is set to 3.
+
+    @param s the Stock for this Node
+    @param prev the pointer to the previous Node
+    @param succ the pointer to the successor Node
+  */ 
+  StockList( Stock s, Node *prev, Node *succ );
+
+  /**
+    Default constructor for a StockList
   */
-  Stock *insert( Stock *n, std::string p );
+  StockList(){}
+
+  /**
+    Adds this Node at the end of a StockList.
+
+    @param n the Node to be inserted
+    @return the head Node
+  */
+  Node *insertAtEnd( Node *n );
 
   /**
     Adds a Stock to the front of the list and returns a pointer to the Stock that was added.
@@ -41,7 +66,7 @@ class StockList{
     @param n the Stock to add
     @return a pointer to the newly added Stock
   */
-  Stock *add( Stock *n );
+  Node *add( Node *n );
 
   /**
     Locates the first stock in the list with the price point described by the string parameter
@@ -49,7 +74,7 @@ class StockList{
     @param p the price point is either open, close, previous close, high, or low
     @return the target Stock
   */
-  Stock *find( Stock *t, std::string p);
+  Node *find( Node *t, std::string p);
 
   /**
     Sorts the list by the price point described by the parameter. The different points are close, previous close, open, high and low. Be default the list is sorted
@@ -58,19 +83,70 @@ class StockList{
     @param p description of the price point
     @return a pointer to the head
   */
-  Stock *sortByPrice( std::string p );
+  Node *sortByPrice( std::string p );
 
-  bool isEmpty() { if(size==0) return true; }
+  /**
+    Verifies that the list contains no elements.
+    @return true if there are no elements in the list
+  */
+  bool isEmpty() { if(length==0) return true; }
 
-  Stock *insertAtFront( Stock *n );
+  /**
+    Add a Node to the front of the list and assigns the head to this Node.
 
-  Stock *insertAtEnd
+    @param n the Node to be added to the front of the list
+    @return a pointer to the head
+  */
+  Node *insertAtFront( Node *n );
+
+  /**
+    Returns a pointer to the last Node in the list
+    @return pointer to the last Node
+  */
+  Node *getLast();
+
+  /**
+    Returns true if the list is currently sorted in ascending order by stock price.
+    @return true if sorted in ascending order
+  */
+  bool isSortedAscending();
   
 
-
 private:
-  Stock *head;
-  Stock *prev;
-  Stock *succ;
+
+  /** 
+    A list node has a Stock and pointers to its successor node and previous node 
+  */
+  class Node{
+  public:
+    /**
+      Constructs a Node with a Stock, pointers to the previous and successor Nodes in the list
+      @param s the Stock
+      @param p a pointer to the previous Node
+      @param sc a pointer to the successor Node
+    */
+    Node ( Stock s, Node *p, Node *sc ) : stock{s}, prev{p}, succ{sc} {}
+
+    /**
+      The default constructor for Node
+    */
+    Node (){}
+  private:
+    /**
+      Default constructor
+    */
+    Node(){}
+    /** The stock */
+    Stock stock;
+    /** A pointer to previous Node in the list */
+    Node *prev;
+    /** A pointer to the successor Node in the list */
+    Node *succ;
+  };
+
+  /** List length */
   int length;
+  /** The first node in the list */
+  Node *head;
+ 
 };
